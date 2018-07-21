3a410dbea534213cd4d0e1f39dd7c079d971aa85
maximus1998g@mail.ru <maximus1998g@mail.ru>
2018-07-22 02:38:42 +0900

    add reverse('') instead of urlsin views & add README.md

--- loginsys/views.py
+++ loginsys/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
-from django.utils.http import is_safe_url
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import REDIRECT_FIELD_NAME, login, logout, authenticate
 from django.utils.decorators import method_decorator
@@ -9,10 +7,11 @@ from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.generic import FormView, RedirectView, CreateView
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 
 
 class LoginView(FormView):
-    success_url = '/profile/me'
     form_class = AuthenticationForm
     template_name = 'login.html'
     redirect_field_name = REDIRECT_FIELD_NAME
@@ -21,26 +20,17 @@ class LoginView(FormView):
     @method_decorator(csrf_protect)
     @method_decorator(never_cache)
     def dispatch(self, request, *args, **kwargs):
-        # Sets a test cookie to make sure the user has cookies enabled
         request.session.set_test_cookie()
-
         return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         login(self.request, form.get_user())
-
-        # If the test cookie worked, go ahead and
-        # delete it since its no longer needed
         if self.request.session.test_cookie_worked():
             self.request.session.delete_test_cookie()
-
         return super().form_valid(form)
 
     def get_success_url(self):
-        redirect_to = self.request.GET.get(self.redirect_field_name)
-        if not is_safe_url(url=redirect_to, host=self.request.get_host()):
-            redirect_to = self.success_url
-        return redirect_to
+        return reverse('profile')
 
 
 class LogoutView(RedirectView):
@@ -54,7 +44,6 @@ class LogoutView(RedirectView):
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = '/profile/me'
     template_name = 'register.html'
 
     def form_valid(self, form):
@@ -68,7 +57,7 @@ class SignUpView(CreateView):
         login(self.request, user)
 
         # add a default avatar to new user
-        avatar = Profile.create(self.request.user.id)
-        avatar.save()
-        return redirect('/profile/me')
+        profile = Profile.create(self.request.user.id)
+        profile.save()
+        return redirect(reverse('profile'))
 
